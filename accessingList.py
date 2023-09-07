dff5111c5a6673c7d445744761c84d0d8be17275
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-07 01:36:31 -0400

    Create accessingList.py

---
+++ accessingList.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "orange"]
+
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+third_fruit = fruits[2]
+
+print(first_fruit)   # Output: "apple"
+print(second_fruit)  # Output: "banana"
+print(third_fruit)   # Output: "orange"
