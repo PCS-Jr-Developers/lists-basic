f188f375f1139c8c46dc37e1654460f7d0398f27
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2024-02-28 11:10:52 -0500

    Update editingList.py

--- editingList.py
+++ editingList.py
@@ -1,3 +1,7 @@
 fruits = ["apple", "banana", "orange"]
 fruits.append("kiwi")
 print(fruits)  # Output: ['apple', 'banana', 'orange', 'kiwi']
+
+fruits = ['apple', 'banana', 'orange', 'kiwi']
+fruits.remove('banana')
+print(fruits)  # Output: ['apple', 'orange', 'kiwi']
